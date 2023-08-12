f6e3b7c87101cf8e93f649d94e600de8eac161d2
dablenparty <hdavenport8902@gmail.com>
2023-08-12 18:41:15 -0400

    Overtaker now auto-loads with the server

--- Overtaker.cs
+++ Overtaker.cs
@@ -1,5 +1,23 @@
-﻿namespace OvertakerPlugin;
+﻿using AssettoServer.Server.Plugin;
+using AssettoServer.Shared.Services;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 
-public class Overtaker
+namespace OvertakerPlugin;
+
+public class Overtaker : CriticalBackgroundService, IAssettoServerAutostart
 {
+    public Overtaker(OvertakerConfiguration configuration, IHostApplicationLifetime applicationLifetime) : base(
+        applicationLifetime)
+    {
+        Log.Debug("Overtaker plugin starting with minimum speed {MinimumSpeedKmh} km/h",
+            configuration.MinimumSpeedKmh);
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        // basically, this is the main loop of the plugin
+        // from what I can tell, it's equivalent to acMain() in the Python sdk
+        return Task.CompletedTask;
+    }
 }
---
+++ OvertakerModule.cs
@@ -0,0 +1,12 @@
+﻿using AssettoServer.Server.Plugin;
+using Autofac;
+
+namespace OvertakerPlugin;
+
+public class OvertakerModule : AssettoServerModule<OvertakerConfiguration>
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<Overtaker>().AsSelf().As<IAssettoServerAutostart>().SingleInstance();
+    }
+}
