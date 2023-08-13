542434c5dcc2206db8554b1492b37c403075e528
dablenparty <hdavenport8902@gmail.com>
2023-08-13 00:36:43 -0400

    Created Actions.FixedSizedQueue.cs

---
+++ Actions/FixedSizedQueue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Concurrent;
+
+namespace OvertakerPlugin.Actions;
+
+internal class FixedSizedQueue<T> : ConcurrentQueue<T>
+{
+    public int Size { get; init; }
+    
+    public FixedSizedQueue(int size)
+    {
+        Size = size;
+    }
+    
+    public new void Enqueue(T value)
+    {
+        base.Enqueue(value);
+        while (Count > Size)
+            TryDequeue(out _);
+    }
+}
