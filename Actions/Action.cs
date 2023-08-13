2b7415934271dcfbf1bbf6ffa96cdb97c958f43f
dablenparty <hdavenport8902@gmail.com>
2023-08-13 15:53:15 -0400

    Created Action.cs

---
+++ Actions/Action.cs
@@ -0,0 +1,16 @@
+﻿namespace OvertakerPlugin.Actions;
+
+public abstract class Action
+{
+    protected readonly OvertakerConfiguration Configuration;
+    public abstract string Name { get; init; }
+    public abstract int Value { get; init; }
+
+
+    protected Action(OvertakerConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public abstract int ScoreAction(List<TickState> stateHistory);
+}
