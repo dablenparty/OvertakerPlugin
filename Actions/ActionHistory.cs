2dcba9918356636e280b134d099aaced246850c2
dablenparty <hdavenport8902@gmail.com>
2023-08-13 15:53:35 -0400

    Created ActionHistory, OvertakeAction

---
+++ Actions/ActionHistory.cs
@@ -0,0 +1,14 @@
+﻿using Serilog;
+
+namespace OvertakerPlugin.Actions;
+
+public class ActionHistory
+{
+    private readonly ILogger _logger = Log.ForContext<ActionHistory>();
+    private readonly Dictionary<string, Action> _registeredActions = new();
+
+    public ActionHistory(OvertakerConfiguration configuration)
+    {
+        _registeredActions.Add("Overtake", new OvertakeAction(configuration));
+    }
+}
---
+++ Actions/OvertakeAction.cs
@@ -0,0 +1,36 @@
+﻿using System.Numerics;
+
+namespace OvertakerPlugin.Actions;
+
+public class OvertakeAction : Action
+{
+    public sealed override string Name { get; init; } = "Overtake";
+    public sealed override int Value { get; init; } = 100;
+
+    public override int ScoreAction([StateHistory.NeedsHistory(StateCount = 2)] List<TickState> stateHistory)
+    {
+        var thisTick = stateHistory[0];
+        var lastTick = stateHistory[1];
+        foreach (var (_, entryCar) in thisTick.EntryCars)
+        {
+            var speedKmh = OvertakerUtils.KmhFromAcVelocity(entryCar.Status.Velocity);
+            if (entryCar.AiControlled || speedKmh < Configuration.MinimumSpeedKmh)
+                return 0;
+            var nearbyCars = thisTick.EntryCars
+                .Select(p => p.Value)
+                .Where(c => c != entryCar &&
+                            Vector3.Distance(c.Status.Position, entryCar.Status.Position) <=
+                            Configuration.MinimumDistanceMeters)
+                .ToList();
+            if (nearbyCars.Count == 0)
+                return 0;
+        }
+
+        // TODO: the rest of this
+        return -1;
+    }
+
+    public OvertakeAction(OvertakerConfiguration configuration) : base(configuration)
+    {
+    }
+}
