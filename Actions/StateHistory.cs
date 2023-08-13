eb77868e8406abb77e77fd8d930c736798870cda
dablenparty <hdavenport8902@gmail.com>
2023-08-13 13:16:32 -0400

    Implemented state history

---
+++ Actions/StateHistory.cs
@@ -0,0 +1,19 @@
+﻿using AssettoServer.Server;
+using Serilog;
+
+namespace OvertakerPlugin.Actions;
+
+internal class StateHistory
+{
+    private readonly ILogger _logger = Log.ForContext<StateHistory>();
+    internal FixedSizedQueue<TickState> TickStates { get; private init; } = new(100);
+    
+    internal TickState this[int idx] => TickStates.ElementAt(idx);
+
+    internal TickState NewTickHappened(EntryCarManager entryCarManager)
+    {
+        var tickState = new TickState(entryCarManager.EntryCars, DateTime.Now);
+        TickStates.Enqueue(tickState);
+        return tickState;
+    }
+}
---
+++ Actions/TickState.cs
@@ -0,0 +1,25 @@
+﻿using AssettoServer.Server;
+
+namespace OvertakerPlugin.Actions;
+
+internal readonly struct TickState
+{
+    public Dictionary<string, EntryCar> EntryCars { get; init; }
+    public DateTime TimeOfTick { get; init; }
+
+    public TickState(IEnumerable<EntryCar> entryCars, DateTime timeOfTick)
+    {
+        EntryCars = entryCars.ToDictionary(c => c.Client?.HashedGuid ?? c.Model);
+        TimeOfTick = timeOfTick;
+    }
+
+    /// <summary>
+    /// Funky method to get a car from a car. This is used by the event loop to get car data from multiple TickState's
+    /// </summary>
+    /// <param name="car"></param>
+    /// <returns></returns>
+    public EntryCar? GetCarByCar(EntryCar car)
+    {
+        return EntryCars.TryGetValue(car.Client?.HashedGuid ?? car.Model, out var entryCar) ? entryCar : null;
+    }
+}
