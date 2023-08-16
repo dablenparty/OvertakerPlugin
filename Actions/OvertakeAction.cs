894c1b8adf04a1c676889e67326899b43abd2bc3
dablenparty <hdavenport8902@gmail.com>
2023-08-16 17:13:55 -0400

    Replaced broken code with placeholder

--- Actions/OvertakeAction.cs
+++ Actions/OvertakeAction.cs
@@ -1,5 +1,6 @@
 ﻿using System.Numerics;
 using OvertakerPlugin.State;
+using Serilog;
 
 namespace OvertakerPlugin.Actions;
 
@@ -8,27 +9,12 @@ public class OvertakeAction : Action
     public sealed override string Name { get; init; } = "Overtake";
     public sealed override int Value { get; init; } = 100;
 
-    public override int ScoreAction([StateHistory.NeedsHistory(StateCount = 2)] List<TickState> stateHistory)
+    public override Dictionary<string, uint> ScoreAction(
+        [StateHistory.NeedsHistory(StateCount = 2)]
+        List<TickState> stateHistory)
     {
-        var thisTick = stateHistory[0];
-        var lastTick = stateHistory[1];
-        foreach (var (_, entryCar) in thisTick.EntryCars)
-        {
-            var speedKmh = OvertakerUtils.KmhFromAcVelocity(entryCar.Status.Velocity);
-            if (entryCar.AiControlled || speedKmh < Configuration.MinimumSpeedKmh)
-                return 0;
-            var nearbyCars = thisTick.EntryCars
-                .Select(p => p.Value)
-                .Where(c => c != entryCar &&
-                            Vector3.Distance(c.Status.Position, entryCar.Status.Position) <=
-                            Configuration.MinimumDistanceMeters)
-                .ToList();
-            if (nearbyCars.Count == 0)
-                return 0;
-        }
-
         // TODO: the rest of this
-        return -1;
+        return new Dictionary<string, uint>();
     }
 
     public OvertakeAction(OvertakerConfiguration configuration) : base(configuration)
