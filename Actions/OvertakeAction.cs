7be4e5be3dd77a67655a5a8d665e12f6e0f607d7
dablenparty <hdavenport8902@gmail.com>
2023-08-16 20:23:02 -0400

    Implemented basic overtake scoring

--- Actions/OvertakeAction.cs
+++ Actions/OvertakeAction.cs
@@ -1,4 +1,5 @@
-﻿using OvertakerPlugin.State;
+﻿using System.Numerics;
+using OvertakerPlugin.State;
 
 namespace OvertakerPlugin.Actions;
 
@@ -15,7 +16,58 @@ public class OvertakeAction : AbstractOvertakerAction
         [StateHistory.NeedsHistory(StateCount = 2)]
         List<TickState> stateHistory)
     {
-        // TODO: the rest of this
-        return new Dictionary<byte, uint>();
+        var scoreUpdates = new Dictionary<byte, uint>();
+
+        var thisTick = stateHistory[0];
+        var prevTick = stateHistory[1];
+
+        foreach (var (_, currentCarState) in thisTick.CarStates)
+        {
+            if (currentCarState.AiControlled || currentCarState.SpeedKmh < Configuration.MinimumSpeedKmh)
+                continue;
+            var nearbyCars = thisTick.CarStates.Where(p =>
+            {
+                var (sessionId, carState) = p;
+                var distance = Vector3.Distance(carState.Position, currentCarState.Position);
+                return sessionId != currentCarState.SessionId && 0 < distance &&
+                       distance <= Configuration.MinimumDistanceMeters;
+            }).ToList();
+
+            if (nearbyCars.Count == 0)
+                continue;
+
+            var overtakes = 0;
+            var distancesToMe = new List<float>();
+            var speedDifferences = new List<float>();
+            var prevCarState = prevTick[currentCarState.SessionId];
+            foreach (var (_, nearCar) in nearbyCars)
+            {
+                var prevNearCarState = prevTick[nearCar.SessionId];
+                var relPosBefore = prevNearCarState.GetRelativePositionTo(prevCarState);
+                var relPosAfter = nearCar.GetRelativePositionTo(currentCarState);
+                // was beside, now behind
+                if (relPosBefore != RelativePosition.Sides || relPosAfter != RelativePosition.Behind)
+                    continue;
+                var distanceToCurrent = Vector3.Distance(nearCar.Position, currentCarState.Position);
+                distancesToMe.Add(distanceToCurrent);
+                var speedDifference = nearCar.SpeedKmh - currentCarState.SpeedKmh;
+                speedDifferences.Add(speedDifference);
+                overtakes++;
+            }
+
+            if (overtakes == 0)
+                continue;
+
+            var distancesAvg = distancesToMe.Average();
+            var speedDiffsAvg = speedDifferences.Average();
+            var score = (uint) (Value * overtakes + distancesAvg + speedDiffsAvg +
+                                currentCarState.SpeedKmh);
+            if (scoreUpdates.TryGetValue(currentCarState.SessionId, out var existingScore))
+                scoreUpdates[currentCarState.SessionId] = existingScore + score;
+            else
+                scoreUpdates.Add(currentCarState.SessionId, score);
+        }
+
+        return scoreUpdates;
     }
 }
