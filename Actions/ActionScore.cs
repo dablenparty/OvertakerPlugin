b9b041afdc096001753103169164b96eb57d7c0c
dablenparty <hdavenport8902@gmail.com>
2023-08-16 22:24:33 -0400

    Created ActionScore

---
+++ Actions/ActionScore.cs
@@ -0,0 +1,7 @@
+﻿namespace OvertakerPlugin.Actions;
+
+public readonly struct ActionScore
+{
+    public required uint Score { get; init; }
+    public required DateTime HappenedAt { get; init; }
+}
